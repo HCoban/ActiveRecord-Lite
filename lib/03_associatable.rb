0432318d0a19a07917e0aa655be9ca28dfc4686c
Halil Coban <halil.coban@gmail.com>
2016-08-10 15:04:54 -0700

    Implemented ::belongs_to and ::has_many in Associatable

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,34 +10,64 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    self.model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      primary_key: :id,
+      foreign_key: "#{name}_id".to_sym,
+      class_name: name.to_s.camelcase
+    }
+
+    options = defaults.merge(options)
+
+    options.each do |variable, value|
+      instance_variable_set("@#{variable}", value)
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      primary_key: :id,
+      class_name: name.to_s.singularize.camelcase,
+      foreign_key: "#{self_class_name.downcase}_id".to_sym
+    }
+
+    options = defaults.merge(options)
+
+    options.each do |variable, value|
+      instance_variable_set("@#{variable}", value)
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method("#{name}") do
+      options.model_class
+        .where(options.primary_key => send(options.foreign_key))
+        .first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+    define_method("#{name}") do
+      options.model_class
+        .where(options.foreign_key => self.send(options.primary_key))
+    end
   end
 
   def assoc_options
@@ -46,5 +76,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
